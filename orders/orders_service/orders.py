e77b347eb70c2a5a9d4c391acc756433c41150ba
Zain Haq <zainhaq1026@gmail.com>
2024-03-18 15:58:33 -0400

    authentication & authorization

--- orders/orders_service/orders.py
+++ orders/orders_service/orders.py
@@ -2,7 +2,7 @@ import requests
 
 from orders.orders_service.exceptions import (
     APIIntegrationError,
-    InvalidActionError
+    InvalidActionError,
 )
 
 
@@ -15,15 +15,23 @@ class OrderItem:
 
     def dict(self):
         return {
-            'product': self.product,
-            'size': self.size,
-            'quantity': self.quantity
+            "product": self.product,
+            "size": self.size,
+            "quantity": self.quantity,
         }
 
 
 class Order:
-    def __init__(self, id, created, items, status, schedule_id=None,
-                 delivery_id=None, order_=None):
+    def __init__(
+        self,
+        id,
+        created,
+        items,
+        status,
+        schedule_id=None,
+        delivery_id=None,
+        order_=None,
+    ):
         self._order = order_
         self._id = id
         self._created = created
@@ -45,44 +53,41 @@ class Order:
         return self._status or self._order.status
 
     def cancel(self):
-        if self.status == 'progress':
+        if self.status == "progress":
+            kitchen_base_url = "http://localhost:3000/kitchen"
             response = requests.post(
-                f'http://localhost:3000/kitchen/schedules/{self.schedule_id}/cancel',
-                json={'order': [item.dict() for item in self.items]}
+                f"{kitchen_base_url}/schedules/{self.schedule_id}/cancel",
+                json={"order": [item.dict() for item in self.items]},
             )
             if response.status_code == 200:
                 return
-            raise APIIntegrationError(
-                f'Could not cancel order with id {self.id}'
-            )
-        if self.status == 'delivery':
-            raise InvalidActionError(f'Cannot cancel order with id {self.id}')
+            raise APIIntegrationError(f"Could not cancel order with id {self.id}")
+        if self.status == "delivery":
+            raise InvalidActionError(f"Cannot cancel order with id {self.id}")
 
     def pay(self):
         response = requests.post(
-            'http://localhost:3001/payments', json={'order_id': self.id}
+            "http://localhost:3001/payments", json={"order_id": self.id}
         )
         if response.status_code == 201:
             return
         raise APIIntegrationError(
-            f'Could not process payment for order with id {self.id}'
+            f"Could not process payment for order with id {self.id}"
         )
 
     def schedule(self):
         response = requests.post(
-            'http://localhost:3000/kitchen/schedules',
-            json={'order': [item.dict() for item in self.items]}
+            "http://localhost:3000/kitchen/schedules",
+            json={"order": [item.dict() for item in self.items]},
         )
         if response.status_code == 201:
-            return response.json()['id']
-        raise APIIntegrationError(
-            f'Could not schedule order with id {self.id}'
-        )
+            return response.json()["id"]
+        raise APIIntegrationError(f"Could not schedule order with id {self.id}")
 
     def dict(self):
         return {
-            'id': self.id,
-            'order': [item.dict() for item in self.items],
-            'status': self.status,
-            'created': self.created,
+            "id": self.id,
+            "order": [item.dict() for item in self.items],
+            "status": self.status,
+            "created": self.created,
         }
--- orders/web/api/api.py
+++ orders/web/api/api.py
@@ -3,6 +3,7 @@ from uuid import UUID
 
 from fastapi import HTTPException
 from starlette import status
+from starlette.requests import Request
 from starlette.responses import Response
 
 from orders.orders_service.exceptions import OrderNotFoundError
@@ -18,11 +19,17 @@ from orders.web.api.schemas import (
 
 
 @app.get("/orders", response_model=GetOrdersSchema)
-def get_orders(cancelled: Optional[bool] = None, limit: Optional[int] = None):
+def get_orders(
+    request: Request,
+    cancelled: Optional[bool] = None,
+    limit: Optional[int] = None,
+):
     with UnitOfWork() as unit_of_work:
         repo = OrdersRepository(unit_of_work.session)
         orders_service = OrdersService(repo)
-        results = orders_service.list_orders(limit=limit, cancelled=cancelled)
+        results = orders_service.list_orders(
+            limit=limit, cancelled=cancelled, user_id=request.state.user_id
+        )
     return {"orders": [result.dict() for result in results]}
 
 
@@ -31,26 +38,28 @@ def get_orders(cancelled: Optional[bool] = None, limit: Optional[int] = None):
     status_code=status.HTTP_201_CREATED,
     response_model=GetOrderSchema,
 )
-def create_order(payload: CreateOrderSchema):
+def create_order(request: Request, payload: CreateOrderSchema):
     with UnitOfWork() as unit_of_work:
         repo = OrdersRepository(unit_of_work.session)
         orders_service = OrdersService(repo)
         order = payload.dict()["order"]
         for item in order:
             item["size"] = item["size"].value
-        order = orders_service.place_order(order)
+        order = orders_service.place_order(order, request.state.user_id)
         unit_of_work.commit()
         return_payload = order.dict()
     return return_payload
 
 
 @app.get("/orders/{order_id}", response_model=GetOrderSchema)
-def get_order(order_id: UUID):
+def get_order(request: Request, order_id: UUID):
     try:
         with UnitOfWork() as unit_of_work:
             repo = OrdersRepository(unit_of_work.session)
             orders_service = OrdersService(repo)
-            order = orders_service.get_order(order_id=order_id)
+            order = orders_service.get_order(
+                order_id=order_id, user_id=request.state.user_id
+            )
         return order.dict()
     except OrderNotFoundError:
         raise HTTPException(
@@ -59,7 +68,7 @@ def get_order(order_id: UUID):
 
 
 @app.put("/orders/{order_id}", response_model=GetOrderSchema)
-def update_order(order_id: UUID, order_details: CreateOrderSchema):
+def update_order(request: Request, order_id: UUID, order_details: CreateOrderSchema):
     try:
         with UnitOfWork() as unit_of_work:
             repo = OrdersRepository(unit_of_work.session)
@@ -67,7 +76,11 @@ def update_order(order_id: UUID, order_details: CreateOrderSchema):
             order = order_details.dict()["order"]
             for item in order:
                 item["size"] = item["size"].value
-            order = orders_service.update_order(order_id=order_id, items=order)
+            order = orders_service.update_order(
+                order_id=order_id,
+                items=order,
+                user_id=request.state.user_id,
+            )
             unit_of_work.commit()
         return order.dict()
     except OrderNotFoundError:
@@ -81,12 +94,14 @@ def update_order(order_id: UUID, order_details: CreateOrderSchema):
     status_code=status.HTTP_204_NO_CONTENT,
     response_class=Response,
 )
-def delete_order(order_id: UUID):
+def delete_order(request: Request, order_id: UUID):
     try:
         with UnitOfWork() as unit_of_work:
             repo = OrdersRepository(unit_of_work.session)
             orders_service = OrdersService(repo)
-            orders_service.delete_order(order_id=order_id)
+            orders_service.delete_order(
+                order_id=order_id, user_id=request.state.user_id
+            )
             unit_of_work.commit()
         return
     except OrderNotFoundError:
@@ -96,12 +111,14 @@ def delete_order(order_id: UUID):
 
 
 @app.post("/orders/{order_id}/cancel", response_model=GetOrderSchema)
-def cancel_order(order_id: UUID):
+def cancel_order(request: Request, order_id: UUID):
     try:
         with UnitOfWork() as unit_of_work:
             repo = OrdersRepository(unit_of_work.session)
             orders_service = OrdersService(repo)
-            order = orders_service.cancel_order(order_id=order_id)
+            order = orders_service.cancel_order(
+                order_id=order_id, user_id=request.state.user_id
+            )
             unit_of_work.commit()
         return order.dict()
     except OrderNotFoundError:
@@ -111,12 +128,14 @@ def cancel_order(order_id: UUID):
 
 
 @app.post("/orders/{order_id}/pay", response_model=GetOrderSchema)
-def pay_order(order_id: UUID):
+def pay_order(request: Request, order_id: UUID):
     try:
         with UnitOfWork() as unit_of_work:
             repo = OrdersRepository(unit_of_work.session)
             orders_service = OrdersService(repo)
-            order = orders_service.pay_order(order_id=order_id)
+            order = orders_service.pay_order(
+                order_id=order_id, user_id=request.state.user_id
+            )
             unit_of_work.commit()
         return order.dict()
     except OrderNotFoundError:
